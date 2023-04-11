209030d34c1dd658f0b80391d453ffd164a480d1
Omer Isik <isikomer4545@gmail.com>
2023-04-11 16:41:55 +0200

    css inline and internal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Jsx3 from "./components/02-jsx/jsx3";
 import Jsx4 from "./components/02-jsx/jsx4";
 import Jsx5 from "./components/02-jsx/jsx5";
 import Style1 from "./components/03-styles/style1";
+import Style2 from "./components/03-styles/style2";
 
 const App = () => {
     return (
@@ -25,7 +26,11 @@ const App = () => {
             <br/>
             <Jsx5>
             </Jsx5>
+            <br/>
             <Style1 />
+            <br/>
+            <Style2 />
+            <br/>
         </div>
     )
 };
