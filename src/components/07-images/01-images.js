33f8c84d1967c77399f20a2e21ff18a1ab75fef6
Omer Isik <isikomer4545@gmail.com>
2023-04-16 15:17:24 +0200

    different ways of gettin images.

--- src/components/07-images/01-images.js
+++ src/components/07-images/01-images.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import northernLights from "../../assets/img/image1.jpg"
 
 const Images1 = () => {
+    const marginLeft = {
+        marginLeft : "1rem"
+    }
   return (
     <div>
         <img src="./image6.jpg" width={300} alt="image6" title='image6'/>
 
-        <img src="https://picsum.photos/id/2/300/200" alt="computer" title='computer'/>
+        <img src="https://picsum.photos/id/2/300/200" alt="computer" title='computer' style={marginLeft}/>
+
+        <img src={northernLights} width={300} alt="northernLights" title='northernLights'/>
+
+        <img 
+            src={require("../../assets/img/image2.jpg")} 
+            alt=""
+            title=''
+            width={300}
+        />
     </div>
   )
 }
