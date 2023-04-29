a10a841f4212eada6a948b76433c9f14a35e5e90
Omer Isik <isikomer4545@gmail.com>
2023-04-29 17:35:10 +0200

    Fetch mock api

--- IntroToReact/src/components/19-fetch/user-cards.js
+++ IntroToReact/src/components/19-fetch/user-cards.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserCard from './user-card'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const UserCards = () => {
+    const [users, setUsers] = useState([]);
+    const loadData = () =>{
+        let newVar = fetch('https://644d2ac957f12a1d3dd95135.mockapi.io/users')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setUsers(data);
+        });
+    }
+    useEffect(()=>{
+        loadData();
+    }, [])
   return (
     <Container>
         <Row>
             <Col>
-                <UserCard/>
+                {users.map((user)=>{
+                    return(
+                        <UserCard key={user.id}{...user}/>
+                    )
+                })
+                }
             </Col>
         </Row>
     </Container>
