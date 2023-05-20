1b671fdcdc216d5d15f7bf0ca16b4abc6e699568
Omer Isik <isikomer4545@gmail.com>
2023-05-20 07:43:14 +0200

    phone adjusted

--- cevsentracker/src/components/Home.js
+++ cevsentracker/src/components/Home.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import {  Carousel, Container, Form, Table } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react';
+import { Carousel, Container, Form, Table } from 'react-bootstrap';
 import './home.scss';
-import fazilet from '../assets/data/fazilet.json'
-import oku from '../assets/image/oku.jpg'
+import fazilet from '../assets/data/fazilet.json';
+import oku from '../assets/image/oku.jpg';
 
-const Home = (id) => {
+const Home = () => {
   const names = ['Faruk', 'Enes', 'Mustafa', 'Mehmet', 'Ömer']; // İsim verileri
   const dates = ['20-05', '21-05', '22-05', '23-05', '24-05']; // Tarih verileri
 
@@ -13,10 +13,9 @@ const Home = (id) => {
   const handleCheckboxChange = (date, name) => {
     setCheckboxStates((prevState) => ({
       ...prevState,
-      [`${date}-${name}`]: !prevState[`${date}-${name}`]
+      [`${date}-${name}`]: !prevState[`${date}-${name}`],
     }));
   };
-  
 
   const renderCarouselItems = (data) => {
     return data.map((item) => (
@@ -50,43 +49,51 @@ const Home = (id) => {
   }, [checkboxStates]);
 
   return (
-    <Container style={{marginTop:'200px'}}>
-      <h5 className='text'>Risale-i Nur talebelerinin en esaslı virdi Cevşenü’l-Kebirdir. Üstadlarının tavsiyesi ile devamlı okuma gayreti içindedirler.
-         Hatta Bediüzzaman “Cevşenü’l-Kebiri, Hizb-i Nuriyeyi Salavât ile neşri, nurculara ve ehl-i imana büyük bir hizmettir” buyurarak
-         hem okunmasını hem neşrini tavsiye etmişlerdir.</h5>
-         <div style={{ height: '300px' }}>
-        <Carousel interval={5000}>
+    <Container className="home-container">
+      <div className="text-container">
+        <h5 className="text">
+          Risale-i Nur talebelerinin en esaslı virdi Cevşenü’l-Kebirdir. Üstadlarının tavsiyesi ile devamlı okuma gayreti içindedirler.
+          Hatta Bediüzzaman “Cevşenü’l-Kebiri, Hizb-i Nuriyeyi Salavât ile neşri, nurculara ve ehl-i imana büyük bir hizmettir” buyurarak
+          hem okunmasını hem neşrini tavsiye etmişlerdir.
+        </h5>
+      </div>
+
+      <div className="carousel-container">
+        <Carousel interval={1000}>
           {renderCarouselItems(fazilet)}
         </Carousel>
-        </div>
-      
-      <Table striped bordered className="custom-table">
-        <thead>
-          <tr>
-            <th className="table-header">Hafta 1</th>
-            {names.map((name, index) => (
-              <th className="table-header" key={index}>{name}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {dates.map((date, dateIndex) => (
-            <tr key={dateIndex}>
-              <th className="table-date">{date}</th>
-              {names.map((name, nameIndex) => (
-                <td key={nameIndex}>{cb(date, name)}</td>
+      </div>
+
+      <div className="table-container">
+        <Table striped bordered className="custom-table">
+          <thead>
+            <tr>
+              <th className="table-header">Hafta 1</th>
+              {names.map((name, index) => (
+                <th className="table-header" key={index}>{name}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </Table>
-      
-      <Container className='table-cell'>
-        <a href="https://www.ortakhatim.com/cevsen/32_33.htm"><img src={oku} alt=""/></a>
-      </Container>
-      
+          </thead>
+          <tbody>
+            {dates.map((date, dateIndex) => (
+              <tr key={dateIndex}>
+                <th className="table-date">{date}</th>
+                {names.map((name, nameIndex) => (
+                  <td key={nameIndex}>{cb(date, name)}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+
+      <div className="image-container">
+        <a href="https://www.ortakhatim.com/cevsen/32_33.htm">
+          <img src={oku} alt="" />
+        </a>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
 export default Home;
